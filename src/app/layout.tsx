8912f96a0a8f03e48784016c76ef7b87bf020369
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-09-27 11:38:45 -0700

    Init nextjs project

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'EDEN Heardle',
+  description: 'Get 6 chances to guess the EDEN song'
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
