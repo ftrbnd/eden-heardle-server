7f42ddcbb2fb7a6a1a318751b76e5e1e11217936
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-09-30 15:07:03 -0700

    Add rules descriptions

--- src/components/modals/RulesModal.tsx
+++ src/components/modals/RulesModal.tsx
@@ -2,7 +2,18 @@ export default function StatsModal() {
   return (
     <dialog id="rules_modal" className="modal modal-bottom sm:modal-middle">
       <div className="modal-box min-w-min">
-        <h3 className="font-bold text-lg">Rules</h3>
+        <h3 className="font-bold text-2xl">How To Play</h3>
+        <div className="py-4">
+          <p className="font-bold text-lg">Guess the song in 6 tries.</p>
+          <ul className="list-disc px-6 py-4">
+            <li>A random song is selected every day and will have a random starting point.</li>
+            <li>Every incorrect guess extends the {"song's"} playback duration by one second.</li>
+            <li>The color of the tiles will change if your guess belongs to the same album.</li>
+          </ul>
+          <h3 className="font-bold text-lg">Examples</h3>
+          <p className="font-bold text-lg highlight bg-primary">TODO: Add song card examples</p>
+          <p className="text-md">A new puzzle is released daily at midnight (Eastern Time). If you {"haven't"} already, you can join our Discord server for reminders.</p>
+        </div>
         <div className="modal-action">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
