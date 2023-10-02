07770746fc73649b5dc6a01b7eb0db4a1f5bd1ed
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-10-01 23:25:25 -0700

    Begin customizing stat modal

--- src/components/modals/LeaderboardModal.tsx
+++ src/components/modals/LeaderboardModal.tsx
@@ -4,6 +4,187 @@ import { useState } from 'react';
 
 type Tab = 'TODAY' | 'WIN_PCT' | 'CUR_STRK' | 'MAX_STRK';
 
+function StatTable({ statType }: { statType: Tab }) {
+  switch (statType) {
+    case 'TODAY':
+    case 'WIN_PCT':
+    case 'CUR_STRK':
+    case 'MAX_STRK':
+  }
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>
+              <div className="flex items-center space-x-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Hart Hagerty</div>
+                  <div className="text-sm opacity-50">United States</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Zemlak, Daniel and Leannon
+              <br />
+              <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+            </td>
+            <td>Purple</td>
+            <th>
+              <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>
+          {/* row 2 */}
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>
+              <div className="flex items-center space-x-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Brice Swyre</div>
+                  <div className="text-sm opacity-50">China</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Carroll Group
+              <br />
+              <span className="badge badge-ghost badge-sm">Tax Accountant</span>
+            </td>
+            <td>Red</td>
+            <th>
+              <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>
+          {/* row 3 */}
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>
+              <div className="flex items-center space-x-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src="/tailwind-css-component-profile-4@56w.png" alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Marjy Ferencz</div>
+                  <div className="text-sm opacity-50">Russia</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Rowe-Schoen
+              <br />
+              <span className="badge badge-ghost badge-sm">Office Assistant I</span>
+            </td>
+            <td>Crimson</td>
+            <th>
+              <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>
+          {/* row 4 */}
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" className="checkbox" />
+              </label>
+            </th>
+            <td>
+              <div className="flex items-center space-x-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src="/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Yancy Tear</div>
+                  <div className="text-sm opacity-50">Brazil</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Wyman-Ledner
+              <br />
+              <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+            </td>
+            <td>Indigo</td>
+            <th>
+              <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>
+        </tbody>
+        {/* foot */}
+        <tfoot>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+            <th></th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
+
+function Tabs({ activeTab, setActiveTab }: { activeTab: Tab; setActiveTab: (tab: Tab) => void }) {
+  return (
+    <div className="tabs flex justify-center">
+      <a onClick={() => setActiveTab('TODAY')} className={`tab tab-xs sm:tab-md tab-bordered ${activeTab === 'TODAY' && 'tab-active'}`}>
+        Today
+      </a>
+      <a onClick={() => setActiveTab('WIN_PCT')} className={`tab tab-xs sm:tab-md tab-bordered ${activeTab === 'WIN_PCT' && 'tab-active'}`}>
+        Win Percentage
+      </a>
+      <a onClick={() => setActiveTab('CUR_STRK')} className={`tab tab-xs sm:tab-md tab-bordered ${activeTab === 'CUR_STRK' && 'tab-active'}`}>
+        Current Streaks
+      </a>
+      <a onClick={() => setActiveTab('MAX_STRK')} className={`tab tab-xs sm:tab-md tab-bordered ${activeTab === 'MAX_STRK' && 'tab-active'}`}>
+        Max Streaks
+      </a>
+    </div>
+  );
+}
+
 export default function LeaderboardModal() {
   const [activeTab, setActiveTab] = useState<Tab>('TODAY');
 
@@ -11,21 +192,8 @@ export default function LeaderboardModal() {
     <dialog id="leaderboard_modal" className="modal modal-bottom sm:modal-middle">
       <div className="modal-box min-w-min">
         <h3 className="font-bold text-lg">Leaderboard</h3>
-        <div className="tabs flex justify-center">
-          <a onClick={() => setActiveTab('TODAY')} className={`tab tab-bordered ${activeTab === 'TODAY' && 'tab-active'}`}>
-            Today
-          </a>
-          <a onClick={() => setActiveTab('WIN_PCT')} className={`tab tab-bordered ${activeTab === 'WIN_PCT' && 'tab-active'}`}>
-            Win Percentage
-          </a>
-          <a onClick={() => setActiveTab('CUR_STRK')} className={`tab tab-bordered ${activeTab === 'CUR_STRK' && 'tab-active'}`}>
-            Current Streaks
-          </a>
-          <a onClick={() => setActiveTab('MAX_STRK')} className={`tab tab-bordered ${activeTab === 'MAX_STRK' && 'tab-active'}`}>
-            Max Streaks
-          </a>
-        </div>
-        <p></p>
+        <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <StatTable statType={activeTab} />
         <div className="modal-action ">
           <form method="dialog" className="flex gap-2">
             {/* if there is a button in form, it will close the modal */}
