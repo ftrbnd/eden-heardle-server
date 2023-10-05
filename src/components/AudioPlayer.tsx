00c89fa6f453976a9bc89f9e2bc35a164eb71743
ftrbnd <giosalas25@gmail.com>
2023-10-05 16:25:03 -0700

    Add progress and time indicators

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import { getGuessedSongs } from '@/lib/songsApi';
 import { IconDefinition, faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useRef, useState } from 'react';
 
 export default function AudioPlayer() {
   const [icon, setIcon] = useState<IconDefinition>(faPlay);
   const audioRef = useRef<HTMLAudioElement>(null);
+
+  const { data: guesses } = useQuery({
+    queryKey: ['guesses'],
+    queryFn: getGuessedSongs
+  });
 
   useEffect(() => {
     audioRef.current?.addEventListener('playing', () => {
@@ -33,11 +40,15 @@ export default function AudioPlayer() {
 
   return (
     <div className="flex flex-col items-center gap-2 w-full">
-      <progress className="progress progress-primary w-full md:w-3/5 xl:w-2/5" value={1} max="6"></progress>
+      <progress className="progress progress-primary w-full md:w-3/5 xl:w-2/5" value={guesses?.length} max="6"></progress>
 
-      <button className="btn btn-ghost" onClick={togglePlayer}>
-        <FontAwesomeIcon icon={icon} className="w-6 h-6" />
-      </button>
+      <div className="flex justify-between w-full md:w-3/5 xl:w-2/5">
+        <kbd className="kbd">00:0{audioRef.current?.currentTime}</kbd>
+        <button className="btn btn-ghost" onClick={togglePlayer}>
+          <FontAwesomeIcon icon={icon} className="w-6 h-6" />
+        </button>
+        <kbd className="kbd">00:06</kbd>
+      </div>
 
       <audio ref={audioRef} className="hidden" src="/daily_song.mp3" />
     </div>
