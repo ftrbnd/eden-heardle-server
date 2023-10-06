c05e8b6c9808b5fe28bb7c1c27f4bf64f7bc2c5d
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-10-06 14:56:08 -0700

    Move analytics back in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <NextThemesProvider>
           <AuthSessionProvider>
             <TanstackProvider>
-              <LocalUserProvider>{children}</LocalUserProvider>
+              <LocalUserProvider>
+                {children} <Analytics />
+              </LocalUserProvider>
             </TanstackProvider>
           </AuthSessionProvider>
         </NextThemesProvider>
