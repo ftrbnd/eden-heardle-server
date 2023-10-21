709c85406801cf4edd896c3228192af47aacc023
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-10-20 23:04:11 -0700

    Improve connection pooling

--- src/components/modals/LeaderboardModal.tsx
+++ src/components/modals/LeaderboardModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { getLeaderboard, getUserStats } from '@/lib/statsApi';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getLeaderboard } from '@/lib/statsApi';
+import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useState } from 'react';
@@ -14,23 +14,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 type Tab = 'TODAY' | 'WIN_PCT' | 'ACC' | 'CUR_STRK' | 'MAX_STRK';
 
 function ProfileColumn({ user }: { user: User }) {
-  const queryClient = useQueryClient();
-
   const showProfileModal = () => {
     const modal = document.getElementById(`profile_${user.id}_modal`) as HTMLDialogElement;
     if (!modal.open) modal.showModal();
   };
 
-  const prefetchUserStats = async () => {
-    await queryClient.prefetchQuery({
-      queryKey: ['stats', user.id],
-      queryFn: () => getUserStats(user.id)
-    });
-  };
-
   return (
     <td>
-      <div onClick={showProfileModal} onMouseOver={prefetchUserStats} className="flex items-center space-x-3 rounded hover:cursor-pointer hover:bg-base-200">
+      <div onClick={showProfileModal} className="flex items-center space-x-3 rounded hover:cursor-pointer hover:bg-base-200">
         <div className="avatar p-2">
           <div className="mask mask-squircle w-8 h-8">
             <Image src={user.image || '/default.png'} alt={`${user.name}'s Avatar`} height={48} width={48} />
