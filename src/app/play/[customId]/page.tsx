2361f12d56c809e9ec691e0a781b07ed8d24d93d
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-11-07 20:31:51 -0800

    Add dynamic metadata

--- src/app/play/[customId]/page.tsx
+++ src/app/play/[customId]/page.tsx
@@ -2,6 +2,36 @@ import RulesModal from '@/components/modals/RulesModal';
 import SettingsModal from '@/components/modals/SettingsModal';
 import CustomHeardlePageContent from './CustomHeardlePageContent';
 import CustomHeardleModal from '@/components/modals/CustomHeardleModal';
+import { Metadata, ResolvingMetadata } from 'next';
+import prisma from '@/lib/db';
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+
+  // fetch data
+  const customHeardle = await prisma.customHeardle.findUnique({
+    where: {
+      id
+    },
+    include: {
+      user: true
+    }
+  });
+  if (!customHeardle) {
+    return {
+      title: 'Custom EDEN Heardle'
+    };
+  }
+
+  return {
+    title: `${customHeardle?.user.name}'s custom EDEN Heardle`
+  };
+}
 
 export default function CustomHeardlePage({ params }: { params: { customId: string } }) {
   return (
