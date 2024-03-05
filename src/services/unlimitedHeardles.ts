a77a6f400d9d7741a694d9ff1dc25832e65d0fe7
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-03-04 22:35:27 -0800

    Update express api routes (#15)
    * Update express api routes

--- src/services/unlimitedHeardles.ts
+++ src/services/unlimitedHeardles.ts
@@ -4,7 +4,7 @@ const UNLIMITED_HEARDLE_ENDPOINT_EXPRESS = process.env.NEXT_PUBLIC_EXPRESS_URL!;
 
 export const getUnlimitedHeardle = async () => {
   try {
-    const response = await fetch(`${UNLIMITED_HEARDLE_ENDPOINT_EXPRESS}/unlimitedHeardle`);
+    const response = await fetch(`${UNLIMITED_HEARDLE_ENDPOINT_EXPRESS}/heardles/unlimited`);
     if (!response.ok) throw new Error('Failed to get Unlimited Heardle');
 
     const { unlimitedHeardle }: { unlimitedHeardle: UnlimitedHeardle } = await response.json();
