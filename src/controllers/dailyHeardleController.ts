9d083ab4152532019907d8818b159a45a61d5cba
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-05-06 17:22:43 -0700

    Remove comment

--- src/controllers/dailyHeardleController.ts
+++ src/controllers/dailyHeardleController.ts
@@ -2,11 +2,7 @@ import { Request, Response } from 'express';
 import { Heardle, logger } from '../utils/logger';
 import { setDailySong } from '../helpers/heardleGenerators';
 
-// TODO: fix
-// Can't reach database server at `aws-0-us-west-1.pooler.supabase.com`:`5432`
-// Please make sure your database server is running at `aws-0-us-west-1.pooler.supabase.com`:`5432`.
-
-const retryDailyHeardle = async (req: Request, res: Response) => {
+const retryDailyHeardle = async (_req: Request, res: Response) => {
   try {
     logger(Heardle.Daily, 'Retry request received');
     await setDailySong();
