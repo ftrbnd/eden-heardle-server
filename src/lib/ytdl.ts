67814d760003442db0e51e1a47884304e635bede
ftrbnd <giosalas25@gmail.com>
2024-08-08 12:44:16 -0700

    Add ytdl proxy agent

---
+++ src/lib/ytdl.ts
@@ -0,0 +1,13 @@
+import ytdl from '@distube/ytdl-core';
+import { env } from '../utils/env';
+
+const proxies = env.PROXY_URIS;
+
+const getRandomProxy = () => {
+  const index = Math.floor(Math.random() * proxies.length);
+  const proxy = proxies[index];
+
+  return `http://${proxy}`;
+};
+
+export const ytdlProxyAgent = ytdl.createProxyAgent({ uri: getRandomProxy() });
